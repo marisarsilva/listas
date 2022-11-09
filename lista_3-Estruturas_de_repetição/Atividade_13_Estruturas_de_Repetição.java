fb669650da23b5517d90203aa9891e8e41d6c85f
Marisa Rodrigues da Silva <marisa.dev22@gmail.com>
2022-11-09 21:48:54 +0000

    a

---
+++ lista_3-Estruturas_de_repetição/Atividade_13_Estruturas_de_Repetição.java
@@ -0,0 +1,18 @@
+*/13. Uma empresa decidiu fazer um levantamento em relação aos candidatos que se apresentarem
+para preenchimento de vagas no seu quadro de funcionários. Suponha que você seja o
+programador dessa empresa, criar um programa que leia para cada candidato a idade, o sexo
+e se tem experiência no serviço (S ou N). Para encerrar o programa, digite zero para idade.
+Calcule e escreva:
+ O número de candidatos do sexo feminino;
+ O número de candidatos do sexo masculino;*/
+
+
+public class MyClass {
+    public static void main(String args[]) {
+      int x=10;
+      int y=25;
+      int z=x+y;
+
+      System.out.println("Sum of x+y = " + z);
+    }
+}
