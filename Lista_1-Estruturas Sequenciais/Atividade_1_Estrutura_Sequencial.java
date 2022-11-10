ea5541b1aaf340a28fcc45068ed55d074eb8db96
Marisa Rodrigues da Silva <marisa.dev22@gmail.com>
2022-11-10 16:45:42 +0000

    a

--- Lista_1-Estruturas Sequenciais/Atividade_1_Estrutura_Sequencial.java
+++ Lista_1-Estruturas Sequenciais/Atividade_1_Estrutura_Sequencial.java
@@ -1,5 +1,7 @@
 /*1. A imobiliária Imóbilis vende apenas terrenos retangulares. Faça um algoritmo para ler as
-dimensões de um terreno e depois exibir a área do terreno.*/
+dimensões de um terreno e depois exibir a área do terreno.
+@Autor:Marisa_Rodrigues_da_Silva.
+*/
 
 public class atividade1  {
     public static void main(String args[]) {
