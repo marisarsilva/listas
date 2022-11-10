cc0eae991f13f502646e8bdd56bb57347a178401
Marisa Rodrigues da Silva <marisa.dev22@gmail.com>
2022-11-10 01:32:50 +0000

    a

--- lista_3-Estruturas_de_repetição/Atividade_12_Estruturas_de_Repetição.java
+++ lista_3-Estruturas_de_repetição/Atividade_12_Estruturas_de_Repetição.java
@@ -9,28 +9,40 @@ negativos e a soma total dos positivos e negativos juntos.
 
 import java.util.Scanner;
 
-
 public class Atividade_12_Estruturas_de_Repetição {
 
-   
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int totalNumero, numero;
-        
-        se(numero!=0){
-            System.out.println("insira um número positivo ou negativo");
-            numero= input.nextInt();
+        int numero=0;
+        int totalNumero=0, positivo=0, negativo=0;
+              
+        int i=1; 
+
+        do{
+
+           
+            System.out.print(i+"-insira um número positivo ou negativo: ");
+            numero = input.nextInt();
+
+            if (numero > 0) {
+                
+                positivo+=numero;
+
+            } else if (numero < 0) {
+                negativo+=numero;
+            }
+            totalNumero = totalNumero + numero;
+
+            i++;
+        }while(numero!=0);
             
-            totalNumero=totalNumero+numero;
-
-
-        }
-
         
 
-        System.out.print("A soma dos numeros positivos e negativos juntos é igual a: "+somatotal);
+        System.out.print("A soma dos numeros positivos é igual a: " + positivo+"\n");
+        System.out.print("A soma dos numeros negativos é igual a: " + negativo+"\n");
+        System.out.print("A soma dos numeros positivos e negativos juntos é igual a: " + totalNumero+"\n");
+        input.close();
     }
 
-    private static void se(boolean b) {
-    }
+    
 }
